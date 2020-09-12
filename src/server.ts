8a3c5a22040a1c7a0d55ba9c71ebd3d29aacd71a
wanderson <wanderson.alves.rodrigues@gmail.com>
2020-09-12 17:29:50 -0300

    adição express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,45 @@
+import { Server } from "@overnightjs/core";
+import { Application } from "express";
+import bodyParser from "body-parser";
+
 import "./database";
+import ProfileController from "./controllers/ProfileController";
+import UserController from "./controllers/UserController";
+
+export class SetupServer extends Server {
+  constructor(private port: number = 3000) {
+    super();
+  }
+
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.stupControllers();
+  }
+
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
+
+  private stupControllers(): void {
+    const profileController = new ProfileController();
+    const userController = new UserController();
+
+    this.addControllers([profileController, userController]);
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
+
+  public start(): void {
+    this.app.listen(this.port, () => {
+      console.info("Server listening of port", this.port);
+    });
+  }
+}
+
+(async (): Promise<void> => {
+  const server = new SetupServer();
+  await server.init();
+  server.start();
+})();
